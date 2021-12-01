ff686a30056988ef8ea3dd58481b66b20f0d134e
aaronespinoza <aarone@berkeley.edu>
2021-12-01 10:27:39 -0800

    random event printing to screen

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,7 @@
 //questions should be replaced by i
-
-
 var venueAddress;
-function randomEvent(data){
-    var random = Math.floor(Math.random()*data.events.length);
+function randomEvent(data, random){
+    
     //questionsLocation.textContent= random.valueOf;
     venueAddress = data.events[random].venue.address+" "+
     data.events[random].venue.state+", "+
@@ -13,11 +11,12 @@ function randomEvent(data){
 
 function searchResults(city, date) {
   
- var date= document.querySelector("#dateInput").value
- var cityInput= document.querySelector("#datalistOptions").value
+ //var date= document.querySelector("#dateInput").value;
+var cityInput= document.querySelector("#exampleDataList").value;
 //converts cities with spaces in the name to have a plus instead for url
-//var date= "2021-12-04"
+var date= "2021-12-05";
 //var city= "new+york"
+console.log(cityInput);
 var city= cityInput.split(' ').join('+');
 
 
@@ -28,20 +27,39 @@ var city= cityInput.split(' ').join('+');
       return response.json();
     })
     .then(function (data) {
-      randomEvent(data)
-      console.log(data)
-      //Loop over the data to generate a table, each table row will have a link to the repo url
+      var random = Math.floor(Math.random()*data.events.length);
+      randomEvent(data, random)
+      console.log(data, random)
       
-        printResults(data.events[i]);
+      printResults(data, random);
         
         
       
     });
 }
-<<<<<<< HEAD
+//print event result container
+var eventContainer= document.getElementById("eventPop");
+//PRINT RESULTS function
+function printResults(data, random){
+  console.log(data);
+  //Create elements
+  var title = document.createElement("h3");
+  var eventUrl= document.createElement("a");
+  var eventAddress= document.createElement("p");
+  //Set the text content
+  eventAddress.textContent= venueAddress;
+  title.textContent= data.events[random].title;
+  eventUrl.textContent = 'More Info';
+  //add a link to respective event on seatgeek
+  eventUrl.setAttribute('href', data.events[random].url);
+  //add bootstrap classes
+  eventUrl.classList.add('btn', 'btn-dark');
 
-=======
->>>>>>> main
+  //Appending elements
+  eventContainer.append(title);
+  eventContainer.append(eventUrl);
+  eventContainer.append(eventAddress);
+}
 // event listener for showing event results from location search
 
 var rerollBtn = document.querySelector('#rerollBtn');
@@ -53,4 +71,4 @@ rerollBtn.addEventListener("click", searchResults);
 
 // event listener for mapping route to event
 // needs mappingRoute function
-soundsGoodBtn.addEventListener("click", mappingRoute);
+//soundsGoodBtn.addEventListener("click", mappingRoute);
